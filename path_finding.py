18b588d9a86e69be062b59cf4548122b411e2c8d
BenGreenleaf <bdg32@cam.ac.uk>
2026-02-26 12:36:22 +0000

    Path finding changes

--- path_finding.py
+++ path_finding.py
@@ -2,13 +2,45 @@ import heapq
 import math
 
 #neighbour node, weight
+#all in cm
 
+graph_list = {1: [(2, 10, "north")],
+           2: [(1, 10, "south"), (3, 75.6, "east"), (42, 75.6, "west")],
+           3: [(2, 75.6, "west"), (4, 26.5, "south"), (5, 30.4, "east")],
+           4: [(3, 26.5, "north")],
+           5: [(3, 30.4, "west"), (6, 26.5, "south"), (7, 82.7, "north")],
+           6: [(5, 26.5, "north")],
+           7: [(5, 82.7, "south"), (8, 10, "north")],
+           8: [(7, 10, "south"), (9, 10, "north")],
+           9: [(8, 10, "south"), (10, 9.5, "north")],
+           10: [(9, 9.5, "south"), (11, 10, "north")],
+           11: [(10, 10, "south"), (12, 9.5, "north")],
+           12: [(11, 9.5, "south"), (13, 10, "north")],
+           13: [(12, 10, "south"), (14, 40.8, "north")],
+           14: [(13, 40.8, "south"), (15, 105, "west")],
+           15: [(14, 105, "east"), (16, 139, "south"), (33, 107, "west")],
+           
+           33: [(15, 107, "east"), (40, 41.7, "south")],
+           40: [(33, 41.7, "north"), (34, 9.6, "south")],
+           34: [(40, 9.6, "north"), (35, 9, "south")],
+           35: [(34, 9, "north"), (36, 10, "south")],
+           36: [(35, 10, "north"), (37, 9.7, "south")],
+           37: [(36, 9.7, "north"), (38, 10, "south")],
+           38: [(37, 10, "north"), (39, 9.5, "south")],
+           39: [(38, 9.5, "north"), (41, 81.7, "south")],
+           41: [(39, 81.7, "north"), (44, 27, "south"), (42, 105.5, "east")],
+           44: [(41, 27, "north")],
+           42: [(41, 105.5, "west"), (43, 26.2, "south"), (2, 75.6, "east")],
+           43: [(42, 26.2, "north")],
 
-graph_w = {1: [(2, 0, "north")],
-           2: [(1, 0)]
            }
 
-def dijkstra_shortest_path(graph_w, start, goal):
+graph = {
+    node: {nbr: (dist, direction) for nbr, dist, direction in edges}
+    for node, edges in graph_list.items()
+}
+
+def dijkstra_shortest_path(graph, start, goal):
     dist = {start: 0.0}
     parent = {start: None}
     pq = [(0.0, start)]
@@ -19,7 +51,8 @@ def dijkstra_shortest_path(graph_w, start, goal):
             break
         if d != dist.get(u, float("inf")):
             continue
-        for v, w in graph_w[u]:
+
+        for v, (w, _direction) in graph[u].items():
             nd = d + w
             if nd < dist.get(v, float("inf")):
                 dist[v] = nd
@@ -37,57 +70,27 @@ def dijkstra_shortest_path(graph_w, start, goal):
     path.reverse()
     return path, dist[goal]
 
-def turn_decider(last_node, current_orientation, next_node):
-    for i in graph_w[last_node]:
-        if i[0] == next_node:
-            direction = i[2]
-            break
-    
-    if direction == "north":
-        if current_orientation == "north":
-            return "straight"
-        elif current_orientation == "east":
-            return "left"
-        elif current_orientation == "south":
-            return "180 turn"
-        elif current_orientation == "west":
-            return "right"
-    elif direction == "east":
-        if current_orientation == "north":
-            return "right"
-        elif current_orientation == "east":
-            return "straight"
-        elif current_orientation == "south":
-            return "left"
-        elif current_orientation == "west":
-            return "180 turn"
-    elif direction == "south":
-        if current_orientation == "north":
-            return "180 turn"
-        elif current_orientation == "east":
-            return "right"
-        elif current_orientation == "south":
-            return "straight"
-        elif current_orientation == "west":
-            return "left"
-    elif direction == "west":
-        if current_orientation == "north":
-            return "left"
-        elif current_orientation == "east":
-            return "180 turn"
-        elif current_orientation == "south":
-            return "right"
-        elif current_orientation == "west":
-            return "straight"
-        
-def convert_path_to_actions(path, current_orientation):
+def turn_decider(graph, last_node, current_orientation, next_node):
+    _dist, direction = graph[last_node][next_node]
+
+    turn_map = {
+        "north": {"north": "straight", "east": "left",     "south": "180 turn", "west": "right"},
+        "east":  {"north": "right",    "east": "straight", "south": "left",     "west": "180 turn"},
+        "south": {"north": "180 turn", "east": "right",    "south": "straight", "west": "left"},
+        "west":  {"north": "left",     "east": "180 turn", "south": "right",    "west": "straight"},
+    }
+
+    return turn_map[direction][current_orientation]
+
+def convert_path_to_actions(graph, path, current_orientation):
     actions = []
-    for i in range(1, len(path)-1):
-        last_node = path[i]
-        next_node = path[i+1]
-        action = turn_decider(last_node, current_orientation, next_node)
-        actions.append(action)
-        current_orientation = graph_w[last_node][next_node][2] # update orientation based on the direction of the next node
+    for u, v in zip(path[:-1], path[1:]):
+        actions.append(turn_decider(graph, u, current_orientation, v))
+        _dist, direction = graph[u][v]
+        current_orientation = direction
     return actions
 
-
+path, total_dist = dijkstra_shortest_path(graph, 1, 16)
+print("Shortest path:", path)
+print("Total distance:", total_dist)
+print("Directions:", convert_path_to_actions(graph, path, "north")[1:])

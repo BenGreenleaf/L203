931825a79765ca6b5419bad18647d242f4d5290d
b-enh <bfh27@cam.ac.uk>
2026-03-03 16:53:51 +0000

    added a new cornering method - needs testing

--- motor_control_main.py
+++ motor_control_main.py
@@ -16,6 +16,7 @@ last_dir = 0
 align_ticks = 0
 centre_streak = 0
 reverse_ticks = 0
+turning_mode = 1 #one for original two for the new one as of 3.3.26
 
 def update_error(new_error):
     global error, last_error
@@ -24,7 +25,7 @@ def update_error(new_error):
     
 
 def update_mode(state, mode, phase): # mode is the higher level state of the robot shown in capitals, state is the line sensor states, phase is the sub state of a mode to protect against changing states mid transition (shown in lower case)
-    global last_seen, advance_counter, reverse_ticks
+    global last_seen, advance_counter, reverse_ticks, turning_mode
     
     if mode == "LINE_FOLLOWING":
         if state == (0,0,0,1):
@@ -43,36 +44,86 @@ def update_mode(state, mode, phase): # mode is the higher level state of the rob
             return "LINE_FOLLOWING", None
         
     elif mode == "RIGHT_TURN":
-        if phase == "reversing": #can revert this if it doesnt work
-            if reverse_ticks > 0:
-                return "RIGHT_TURN", "reversing"
-            else:
-                return "RIGHT_TURN", "turning"
+        if turning_mode == 1:
+            if phase == "reversing": #can revert this if it doesnt work
+                if reverse_ticks > 0:
+                    return "RIGHT_TURN", "reversing"
+                else:
+                    return "RIGHT_TURN", "turning"
+                
+            if phase == "turning":
+                if state == (0,1,1,1):
+                    return "RIGHT_TURN", "exiting"
+                else:
+                    return "RIGHT_TURN", "turning"
+            elif phase == "exiting":
+                if state == (0,1,1,0):
+                    return "LINE_FOLLOWING", None
+                else:
+                    return "RIGHT_TURN", "exiting"
         
-        if state == (0,1,1,1) and phase == "turning": 
-            return "RIGHT_TURN", "exiting"
-        elif state == (0,1,1,0) and phase == "exiting":
-            return "LINE_FOLLOWING", None
-        elif state == (0,1,1,1) and phase == "exiting":
-            return "RIGHT_TURN", "exiting"
-        else:
-            return "RIGHT_TURN", "turning"
+        elif turning_mode == 2:
+            if phase == "reversing": 
+                if state == (0,1,1,1):
+                    return "RIGHT_TURN", "turning_start"
+                else: 
+                    return "RIGHT_TURN", "reversing"
+            elif phase == "turning_start":
+                if state in [(0,1,1,0), (0,0,1,0)]:
+                    return "RIGHT_TURN", "turning_end"
+                else:
+                    return "RIGHT_TURN", "turning_start"
+            elif phase == "turning_end":
+                if state == (0,1,1,1):
+                    return "RIGHT_TURN", "exiting"
+                else:
+                    return "RIGHT_TURN", "turning_end"
+            elif phase == "exiting":
+                if state == (0,1,1,0):
+                    return "LINE_FOLLOWING", None
+                else:
+                    return "RIGHT_TURN", "exiting"
 
     elif mode == "LEFT_TURN":
-        if phase == "reversing":
-            if reverse_ticks > 0:
-                return "LEFT_TURN", "reversing"
-            else:
-                return "LEFT_TURN", "turning"
-
-        if state == (1,1,1,0) and phase == "turning":
-            return "LEFT_TURN", "exiting"
-        elif state == (0,1,1,0) and phase == "exiting": 
-            return "LINE_FOLLOWING", None
-        elif state == (1,1,1,0) and phase == "exiting":
-            return "LEFT_TURN", "exiting"
-        else:
-            return "LEFT_TURN", "turning"
+        if turning_mode == 1:
+            if phase == "reversing":
+                if reverse_ticks > 0:
+                    return "LEFT_TURN", "reversing"
+                else:
+                    return "LEFT_TURN", "turning"
+                
+            if phase == "turning":
+                if state == (1,1,1,0):
+                    return "LEFT_TURN", "exiting"
+                else:
+                    return "LEFT_TURN", "turning"
+            elif phase == "exiting":
+                if state == (0,1,1,0): # might need to account for what happens when we jump from 0111 to 0100
+                    return "LINE_FOLLOWING", None
+                else:
+                    return "LEFT_TURN", "exiting"
+                
+        elif turning_mode == 2:
+            if phase == "reversing": 
+                if state == (1,1,1,0):
+                    return "LEFT_TURN", "turning_start"
+                else: 
+                    return "LEFT_TURN", "reversing"
+            elif phase == "turning_start":
+                if state in [(0,1,1,0), (0,1,0,0)]:
+                    return "LEFT_TURN", "turning_end"
+                else:
+                    return "LEFT_TURN", "turning_start"
+            elif phase == "turning_end":
+                if state == (1,1,1,0):
+                    return "LEFT_TURN", "exiting"
+                else:
+                    return "LEFT_TURN", "turning_end"
+            elif phase == "exiting":
+                if state == (0,1,1,0):
+                    return "LINE_FOLLOWING", None
+                else:
+                    return "LEFT_TURN", "exiting"
         
     # elif mode == "FIND_LINE":
 
@@ -130,7 +181,7 @@ def update_actions(state, mode, phase):
     turn_speed = 60 # test and adjust
     correction_speed = 10
 
-#variation 1 of the turning code
+#variation 1 of the turning code - old
 
     # if mode == "RIGHT_TURN":
     #     if phase == "turning":
@@ -153,31 +204,60 @@ def update_actions(state, mode, phase):
 
 
     if mode == "RIGHT_TURN":
-        if phase == "reversing":
-            if reverse_ticks > 0:
+        if turning_mode == 1:
+            if phase == "reversing":
+                if reverse_ticks > 0:
+                    motor.set_left(int(-0.7*speed))
+                    motor.set_right(int(-0.7*speed))
+                    reverse_ticks -= 1
+            elif phase == "turning":
+                motor.set_left(1*turn_speed)
+                motor.set_right(-1.2*turn_speed)
+            elif phase == "exiting":
+                motor.set_left(speed)
+                motor.set_right(speed)
+        if turning_mode == 2:
+            if phase == "reversing":
                 motor.set_left(int(-0.7*speed))
                 motor.set_right(int(-0.7*speed))
-                reverse_ticks -= 1
-        elif phase == "turning":
-            motor.set_left(1*turn_speed)
-            motor.set_right(-1.2*turn_speed)
-        elif phase == "exiting":
-            motor.set_left(speed)
-            motor.set_right(speed)
+            elif phase == "turning_start":
+                motor.set_left(turn_speed)
+                motor.set_right(-turn_speed)
+            elif phase == "turning_end":
+                motor.set_left(turn_speed)
+                motor.set_right(-turn_speed) #could even adjust the speed of these values if the curve is too large or too small
+            elif phase == "exiting":
+                motor.set_left(speed)
+                motor.set_right(speed)
 
 
     elif mode == "LEFT_TURN":
-        if phase == "reversing":
-            if reverse_ticks > 0:
+        if turning_mode == 1:
+            if phase == "reversing":
+                if reverse_ticks > 0:
+                    motor.set_left(int(-0.7*speed))
+                    motor.set_right(int(-0.7*speed))
+                    reverse_ticks -= 1
+            elif phase == "turning":
+                motor.set_left(-1.2*turn_speed)
+                motor.set_right(1.05*turn_speed)
+            elif phase == "exiting":
+                motor.set_left(speed)
+                motor.set_right(speed) 
+        
+        if turning_mode == 2:
+            if phase == "reversing":
                 motor.set_left(int(-0.7*speed))
                 motor.set_right(int(-0.7*speed))
-                reverse_ticks -= 1
-        elif phase == "turning":
-            motor.set_left(-1.2*turn_speed)
-            motor.set_right(1.05*turn_speed)
-        elif phase == "exiting":
-            motor.set_left(speed)
-            motor.set_right(speed) 
+            if phase == "turning_start":
+                motor.set_left(-turn_speed)
+                motor.set_right(turn_speed)
+            if phase == "turning_end": #phases look identical but need to separate them as states are contained within each that need to be interpreted differenty
+                motor.set_left(-turn_speed)
+                motor.set_right(turn_speed)
+            if phase == "exiting":
+                motor.set_left(speed)
+                motor.set_right(speed)
 
 
     elif mode == "LINE_FOLLOWING":
